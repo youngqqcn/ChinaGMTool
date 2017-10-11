1ff5b127b9007afca75fe85a5e9cd45d7abf9d60
youngqq <youngqqcn@163.com>
2017-10-11 18:11:17 +0800

    修改菜单优化工具

---
+++ src/RootMenuTool/Check.py
@@ -0,0 +1,60 @@
+#!coding:utf8
+
+'''
+Date:2017/10/11/16:42
+Author:yqq
+Description: 检查菜单是否有错
+'''
+
+import  os
+
+
+
+def Check(fileName, inList):
+	'''
+	:param fileName: 当前正在检查的文件的文件名
+	:param inList: 文件内容
+	:return: 有错返回False, 否则返回True
+	'''
+
+	for i in range(len(inList)):
+		eachLine = inList[i]
+		if i + 1 == len(inList):
+			if "<0x" not in eachLine:
+				print("{0} line{1} : {2}\n".format(fileName , i, inList[i].strip()))
+				return False
+			else:
+				continue
+		if ("<0x" not in eachLine ): #如果没有"<0x", 那么, 它的下一行的tab数一定大于当前行的tab数
+			if inList[i+1].count('\t') <= inList[i].count('\t'):
+				print("{0} line{1} : {2}\n".format(fileName , i, inList[i].strip()))
+				return False
+		else: #如果有"<0x", 那么, 它的下一行的tab一定不大于当前行的tab数
+			if inList[i+1].count('\t') > inList[i].count('\t'):
+				print("{0} line{1} : {2}\n".format(fileName , i, inList[i].strip()))
+				return False
+	pass
+
+
+
+
+
+
+def main():
+
+	menuDir = "../../doc/tmp/RootMenu/"
+	for eachFileName in os.listdir(menuDir) :
+
+		with open(os.path.join(menuDir, eachFileName), "r") as inFile:
+			linesList = inFile.readlines()
+			Check(eachFileName, linesList)
+
+
+	pass
+
+
+if __name__ == "__main__":
+
+	main()
+
+	pass
--- src/RootMenuTool/RootMenuTool.py
+++ src/RootMenuTool/RootMenuTool.py
@@ -13,6 +13,21 @@ gDirName = "../../txt/RootMenu"
 gFilterList = []
 
 
+
+gSpecifyList = [
+	"变速器类型",
+	"牵引力控制",
+	"空调系统",
+	"水平调节系统",
+	"分动箱",
+	"方向盘选件",
+	"安全气囊选件",
+	"前大灯选项",
+	"排气类型",
+	"变速器",
+]
+
+
 # def DelCommantedLine():
 # 	'''
 # 	删除已经屏蔽掉的行, 即 以"//"开头的行
@@ -68,6 +83,116 @@ def Judge(line):
 	return False
 
 
+def IsSpecify(inLine):
+	'''
+	判断是否需要特殊处理
+	:param inLine: 
+	:return: 
+	'''
+
+	for item in gSpecifyList:
+		if item.strip() == inLine.strip():
+			return True
+	return False
+
+
+
+def GetIndex(inList, tabCount):
+	'''
+	:param inList: 
+	:return: 
+	'''
+
+	for i in range(len(inList)):
+		if inList[i].count('\t') == tabCount:
+			return (True, i)
+		if inList[i].count('\t') < tabCount:
+			return (False, -1)
+	return (False, -1)
+
+
+
+
+def SpecifyDeal(inList, begin):
+	'''
+	特殊处理
+	:param inList: 
+	:param begin: 
+	:return: 
+	'''
+
+	'''
+	"变速器类型",
+	"牵引力控制",
+	"空调系统",
+	"水平调节系统",
+	"分动箱",
+	"方向盘选件",
+	"安全气囊选件",
+	"前大灯选项",
+	"排气类型",
+	'''
+
+
+	if inList[begin].strip() == "变速器类型":
+		isSuffix = True
+		suffixStr = "(变速器)"
+		pass
+	elif inList[begin].strip() == "牵引力控制":
+		isSuffix = True
+		suffixStr = "(牵引力控制)"
+		pass
+	elif inList[begin].strip() == "空调系统":
+		isSuffix = True
+		suffixStr = "(空调)"
+		pass
+	elif inList[begin].strip() == "水平调节系统":
+		isSuffix = True
+		suffixStr = "(水平调节)"
+		pass
+	elif inList[begin].strip() == "分动箱":
+		isSuffix = True
+		suffixStr = "(分动箱)"
+		pass
+	elif inList[begin].strip() == "方向盘选件":
+		isSuffix = True
+		suffixStr = "(方向盘选件)"
+		pass
+	elif inList[begin].strip() == "安全气囊选件":
+		isSuffix = True
+		suffixStr = "(安全气囊选件)"
+		pass
+	elif inList[begin].strip() == "前大灯选项":
+		isSuffix = True
+		suffixStr = "(前大灯选项)"
+		pass
+	elif inList[begin].strip() == "排气类型":
+		isSuffix = True
+		suffixStr = "(排气类型)"
+		pass
+	elif inList[begin].strip() == "变速器":   #2016,变速器类型下又分了变速器(AT变速器, EMT变速器)
+		return
+	else:
+		raise ValueError
+
+	tmpStr = inList[begin + 1]
+	if isSuffix: #加后面
+		inList[begin + 1] = tmpStr.replace(tmpStr.strip(), tmpStr.strip() + suffixStr)
+	else: #加前面
+		inList[begin + 1] = tmpStr.replace(tmpStr.strip(), suffixStr + tmpStr.strip() )
+
+	tmpIndex = GetIndex(inList[begin + 2:], inList[begin + 1].count('\t'))
+	if tmpIndex[0]:
+		tmpStr = inList[begin + 2 + tmpIndex[1]]
+		if isSuffix: #加后面
+			inList[begin + 2 + tmpIndex[1]] = tmpStr.replace(tmpStr.strip(), tmpStr.strip() +suffixStr )
+		else: #加前面
+			inList[begin + 2 + tmpIndex[1]] = tmpStr.replace(tmpStr.strip(), suffixStr + tmpStr.strip() )
+	pass
+
+	pass
+
+
 
 def Filter():
 	'''
@@ -86,6 +211,23 @@ def Filter():
 				if Judge(lineList[i]):
 					DelOneTab(lineList, i)   #将从下表i开始, 到tab键小于或等于这一行的tab键的某行结束
 					continue
+				if IsSpecify(lineList[i]):
+					if (lineList[i].strip() == "变速器" ) and ("<0x" in lineList[i + 1]):
+						pass
+					elif (lineList[i].strip() == "分动箱" ) and ("<0x" in lineList[i + 1]):
+						pass
+					elif (lineList[i].strip() == "空调系统" ) and ("<0x" in lineList[i + 1]):
+						pass
+					elif (lineList[i].strip() == "燃油" ) and ("<0x" in lineList[i + 1]):
+						pass
+					elif (lineList[i].strip() == "制动器系统" ) and ("<0x" in lineList[i + 1]):
+						pass
+					elif (lineList[i].strip() == "挂车制动" ) and ("<0x" in lineList[i + 1]):
+						pass
+					else:
+						SpecifyDeal(lineList, i)
+						DelOneTab(lineList, i)   #将从下表i开始, 到tab键小于或等于这一行的tab键的某行结束
+						continue
 				outFile.write(lineList[i])
 
 		outFile.close()
@@ -96,14 +238,15 @@ def Filter():
 
 def main():
 
-	#DelCommantedLine()    #只用一次
 
 
 	with open("../../txt/DelMenuName.txt", "r") as inFile:
 		while True:
 			line  = inFile.readline()
-			if len(line.strip()) == 0:
+			if len(line) == 0:
 				break
+			if len(line.strip()) == 0:
+				continue
 			gFilterList.append(line.strip())
 
 	Filter()
